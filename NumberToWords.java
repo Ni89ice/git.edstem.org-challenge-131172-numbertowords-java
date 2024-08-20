0e9311df7560eff7fbc774e8c5cab7dde8adfc46
Isaac <isaac.shem@student.uts.edu.au>
2024-08-20 13:06:56 +1000

    first commit

---
+++ NumberToWords.java
@@ -0,0 +1,14 @@
+public class NumberToWords {
+
+    public static String intToWords(int num) {
+		//Write your code here 
+    }
+
+    public static String intToRoman(int num) {
+       //Write your code here 
+    }
+
+    public static void main(String[] args) {
+		//Write your code here 
+	}
+}
